21d715eb5c40c718d345f173ef2ec3c64bda573e
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-10-06 13:13:42 +0530

    refactoring of insertion sort to add function as well as method to Sequence class

--- sorting/insertion-sort.cpp
+++ sorting/insertion-sort.cpp
@@ -13,6 +13,16 @@ class Sequence {
             container = new T[size];
         }
 
+        Sequence(const Sequence<T> &to_copy) {
+            size = to_copy.size;
+            container = new T[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                container[i] = to_copy.container[i];
+            }
+        }
+
         T& operator[] (int index) {
             if (index < 1 || index > size) {
                 std::cout << "index: " << index << ", size: " << size << std::endl;
@@ -23,6 +33,24 @@ class Sequence {
         }
         int length() {
             return size;
+        }
+
+        Sequence<T> insertion_sort() {
+            Sequence<T> sorted_sequence = *this;
+            for (int i = 2; i <= sorted_sequence.length(); i++)
+            {
+                int key = sorted_sequence[i];
+                int j = i - 1;
+
+                while (j > 0 && sorted_sequence[j] > key)
+                {
+                    sorted_sequence[j + 1] = sorted_sequence[j];
+                    j = j - 1;
+                }
+                sorted_sequence[j + 1] = key;
+            }
+
+            return sorted_sequence;
         }
 
         std::string to_string() {
@@ -39,6 +67,29 @@ class Sequence {
         }
 };
 
+template <typename T>
+Sequence<T> insertion_sort(const Sequence<T> input_sequence){
+    Sequence<T> sorted_sequence = input_sequence;
+    for (int i = 2; i <= sorted_sequence.length(); i++)
+    {
+        int key = sorted_sequence[i];
+        int j = i - 1;
+
+        while (j > 0 && sorted_sequence[j] > key)
+        {
+            sorted_sequence[j + 1] = sorted_sequence[j];
+            j = j - 1;
+        }
+        sorted_sequence[j + 1] = key;
+    }
+
+    return sorted_sequence;
+}
+// Template in c++
+// Constructors in c++
+// Copy Constructors in c++
+// `this` pointer in c++
+// index operator in c++ - `[]`
 int main()
 {
     try {
@@ -50,22 +101,8 @@ int main()
             std::cin >> A[i];
         }
 
-        std::cout << "Initially: " << A.to_string() << std::endl;
-
-        for (int j = 2; j <= A.length(); j++)
-        {
-            int key = A[j];
-            int i = j - 1;
-
-            while( i > 0 && key < A[i]){
-                A[i + 1] = A[i];
-                i = i - 1;
-            }
-
-            A[i + 1] = key;
-            std::cout << "After step: " << j - 1 << ": " << A.to_string() << std::endl;
-        }
-        std::cout <<"Finally: " << A.to_string() << std::endl;
+        std::cout << "A: " << A.to_string() << std::endl;
+        std::cout <<"Sorted A: " << A.insertion_sort().to_string() << std::endl;
     } catch (const char * e) {
         std::cout << e << std::endl;
     }
